e3ba8b3f1dad56d83ff871416d4ba5ce856a30ff
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-11-05 16:34:15 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"net/http"
+	"time"
+)
+
+type User struct {
+	ID        uint   `gorm:"primaryKey"`
+	Username  string `gorm:"uniqueIndex"`
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
+}
+
+type Register struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func main() {
+	dsn := "host=localhost user=postgres password=testpass123 dbname=nestjs port=4500 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.POST("/local/register", func(c *gin.Context) {
+
+		var req Register
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		encrptedPassword, _ := bcrypt.GenerateFromPassword([]byte(req.Password), 10)
+
+		newUser := User{Username: req.Username, Password: string(encrptedPassword)}
+		result := db.Create(&newUser)
+
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "OK"})
+	})
+
+	router.Run()
+}
